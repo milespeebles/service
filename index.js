66706a26348531ffaff017de0c9beb9bbb5e5f00
anonymous <>
2019-12-23 03:14:21 -0600

    fix

--- index.js
+++ index.js
@@ -69,20 +69,20 @@ const Service = async func => {
         name,
         schema,
       })
-
-      collection.sync ({
-        remote: 'http://localhost:8080/data',
-        waitForLeadership: true,
-        direction: {
-          pull: true,
-          push: true,
-        },
-        options: {
-          live: true,
-          retry: true,
-        },
-      })
     }
+
+    collection.sync ({
+      remote: 'http://localhost:8080/data',
+      waitForLeadership: true,
+      direction: {
+        pull: true,
+        push: true,
+      },
+      options: {
+        live: true,
+        retry: true,
+      },
+    })
 
     return collection
   }
